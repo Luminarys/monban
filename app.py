08da70b23e0d884868500a6d4053f785f5abf5ea
Luminarys <postmaster@gensok.io>
2019-04-05 22:45:47 -0700

    Init commit

---
+++ app.py
@@ -0,0 +1,41 @@
+import json
+import os
+import os.path
+import hashlib
+from glob import glob
+
+config = {}
+with open('config.json') as f:
+        config = json.load(f)
+
+from flask import Flask, send_file
+from flask_basicauth import BasicAuth
+app = Flask(__name__)
+
+app.config['BASIC_AUTH_USERNAME'] = config['user']
+app.config['BASIC_AUTH_PASSWORD'] = config['password']
+dl_dir = config['directory']
+basic_auth = BasicAuth(app)
+
+ids = {}
+
+@app.route("/dl/<string:file_id>")
+def dl(file_id):
+    if file_id in ids:
+        path = ids[file_id]
+        return send_file(path, as_attachment=True, attachment_filename=os.path.basename(path))
+    else:
+        return 'Are you looking for something?', 404
+
+@app.route('/')
+@basic_auth.required
+def secret_view():
+    files = [y for x in os.walk(dl_dir) for y in glob(os.path.join(x[0], '*'))]
+    for f in files:
+        fid = hashlib.sha1((f + config['password']).encode('utf8')).hexdigest()
+        if not fid in ids:
+            ids[fid] = f
+    resp = ''
+    for fid in ids:
+        resp += '<a href="dl/{}">{}</a><br>'.format(fid, ids[fid])
+    return resp
